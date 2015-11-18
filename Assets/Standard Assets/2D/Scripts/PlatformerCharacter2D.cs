457f27a1e61896ef641feed5581168baae94c481
mbonner4th <mbonner4th@gmail.com>
2015-11-17 19:37:42 -0800

    Fix super jump bug.
    The character no longer jumps forever.

--- Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -47,7 +47,8 @@ namespace UnityStandardAssets._2D
 
         private void FixedUpdate()
         {
-            m_Grounded = true;
+            m_Grounded = false;
+            Debug.Log(m_Grounded);
 
             // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
             // This can be done using layers instead but Sample Assets will not overwrite your project settings.
