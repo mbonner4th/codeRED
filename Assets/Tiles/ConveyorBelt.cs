b7b90369f2f6da1b9f21306ad735d3b5cadcdf40
psomisetty <p.somisetty@gmail.com>
2015-12-05 16:35:46 -0800

    Added swamp and conveyor belt

---
+++ Assets/Tiles/ConveyorBelt.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConveyorBelt : MonoBehaviour {
+
+    public bool isRight;
+    public float speed;
+
+    void Awake()
+    {
+        if (!isRight)
+        {
+            speed *= -1;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+	void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Vector3 v = other.transform.position;
+            v.x += speed;
+            other.transform.position = v;
+        }
+    }
+}
