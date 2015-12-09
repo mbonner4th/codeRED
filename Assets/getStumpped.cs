ad114ae1d133c237b5ed4f6c438d656bbce051ac
mbonner4th <mbonner4th@gmail.com>
2015-12-08 18:15:26 -0800

    Finish TRUMP and Eldrich park.
    trump is done and kills the player. Eldrich park is a trash stage
    Changed WeaponManager to use Trump
    put "Huge Loser" sign on jail

---
+++ Assets/getStumpped.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class getStumpped : Weapon{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public override void Effect()
+    {
+
+
+        Object[] o = FindObjectsOfType(typeof(Player));
+        foreach (Player p in o)
+        {
+            if (p.playerNum != this.transform.parent.parent.GetComponent<Player>().playerNum)
+            {
+                GameManager.killPlayer(p);
+            }
+        }
+        Destroy(this.gameObject);
+    }
+
+    public override void Shoot()
+    {
+        if (uses == charges)
+        {
+            return;
+        }
+        uses++;
+        Effect();
+    }
+}
